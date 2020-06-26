a99e951fb48c37889dd41ba2df67e6f81fc15ce4
amarbessedik <amar.bessedik@gmail.com>
2020-06-26 19:57:17 -0400

    MongoDB database created and connection established

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
 
-dotenv.config({path: './config.env'});
+dotenv.config({ path: "./config.env" });
 
+//Middlewares: execute app.use() to add middleware logic for routes or a specific route
+// app.use("/posts", (req, res) => {
+//   console.log("\nHello from middleware");
+//   res.send('Hello from middleware of "localhost:3000/posts" route ')
+// });
+
+//Connect to db
+connectDB();
 
 //Routes
-app.get('/', (req, res) => {
-   res.send('Hello from server!');
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("Hello from posts");
 });
 
 //Server Listening
 PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{console.log('Server listening for ' + PORT)});
+app.listen(PORT, () => {
+  console.log("Server listening for " + PORT);
+});
